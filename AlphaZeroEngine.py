c31442b4e1655d0ef1dee42e392089a7a218a7e3
Victor Sanchez <vsanchez2028@utexas.edu>
2026-06-18 20:34:11 -0500

    Finished simulation step

--- AlphaZeroEngine.py
+++ AlphaZeroEngine.py
@@ -4,6 +4,7 @@ class AlphaZeroEngine():
     SELF_PLAY_BATCH_COUNT = 0
     SIMULATION_COUNT = 0
     EXPLORATION_CONSTANT = 0
+    DNN = None
     board = None
 
     def __init__(self, SELF_PLAY_BATCH_COUNT, SIMULATION_COUNT, EXPLORATION_CONSTANT, board):
@@ -29,7 +30,10 @@ class AlphaZeroEngine():
             self.broadcast_z(game_data)
             games_data.append(game_data)
         return games_data
-    
+
+    def broadcast_z(self, game_data):
+        pass
+
     def calculate_MCTS(self, state):
         tree = {}
         path = [(state, None)]
@@ -40,10 +44,54 @@ class AlphaZeroEngine():
                 if not next_s:
                     path = [(state, None)]
                     break
-        pass
 
     def simulation_step(self, path, tree):
+        if len(path) >= 1:
+            curr_s = path[-1][0]
+        else:
+            return (None, None)
+        
+        legal_moves = self.board.get_legal_moves()
+        if not legal_moves:
+            Z = self.board.get_outcome()
+            self.backprop_MCTS(Z, path)
+            return (None, None)
+        
+        if curr_s in tree:
+            Q_a = tree[curr_s]["Q_a"]
+            N_a = tree[curr_s]["N_a"]
+            P_a = tree[curr_s]["P_a"]
+            N = tree[curr_s]["N"]
+            max_move = None
+            max_val = -np.inf
+            for legal_move in legal_moves:
+                temp = self.calculate_PUCT(Q_a[legal_move], N_a[legal_move], P_a[legal_move], N)
+                if temp > max_val:
+                    max_move = legal_move
+                    max_val = temp
+            new_state = self.play(curr_s, max_move)
+            return (new_state, max_move)
+        
+        Q_a = {}
+        N_a = {}
+        (P_a, V) = self.DNN.forward(curr_s)
+        N = 0
+        for legal_move in legal_moves:
+            Q_a[legal_move] = 0
+            N_a[legal_move] = 0
+        self.backprop(V, path)
+        leaf = {}
+        leaf["Q_a"] = Q_a
+        leaf["N_a"] = N_a
+        leaf["P_a"] = P_a
+        leaf["N"] = N
+        tree[curr_s] = leaf
+        return (None, None)
+
+    def backprop_MCTS(V, path):
         pass
 
-    def broadcast_z(self, game_data):
-        pass
+    def calculate_PUCT(self, Q_sa, N_sa, P_sa, N_s):
+        exploitation = Q_sa
+        exploration = self.EXPLORATION_CONSTANT * P_sa * np.sqrt(N_s/(1 + N_sa))
+        return exploitation + exploration
